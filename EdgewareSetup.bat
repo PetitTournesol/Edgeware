48e8ee22828ec49c7728621ab464998d41552f6b
PetitTournesol <79950428+PetitTournesol@users.noreply.github.com>
2022-02-15 20:03:38 -0600

    Version 2.4.0
    Updating to use py 3.10.2

--- EdgewareSetup.bat
+++ EdgewareSetup.bat
@@ -10,8 +10,8 @@ goto run
 echo Could not find Python.
 echo Now downloading installer from python.org, please wait...
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-if %OS%==32BIT powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.2/python-3.9.2.exe', 'pyinstaller.exe')"
-if %OS%==64BIT powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.2/python-3.9.2-amd64.exe', 'pyinstaller.exe')"
+if %OS%==32BIT powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.2/python-3.10.2.exe', 'pyinstaller.exe')"
+if %OS%==64BIT powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe', 'pyinstaller.exe')"
 echo Done downloading executable.
 echo Please complete installation through the installer before continuing.
 start %CD%\pyinstaller.exe
